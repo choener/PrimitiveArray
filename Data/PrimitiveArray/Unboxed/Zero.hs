0ef051b3534159c44651598cf72555537dea1f88
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2012-01-24 03:58:56 +0100

    added a number of asserts

--- Data/PrimitiveArray/Unboxed/Zero.hs
+++ Data/PrimitiveArray/Unboxed/Zero.hs
@@ -21,35 +21,38 @@ import Data.PrimitiveArray
 
 -- | Monadic arrays of primitive type.
 
-data MArr0 s sh elm = MArr0 {-# UNPACK #-} !sh {-# UNPACK #-} !(MutableByteArray s)
+data MArr0 s sh elm = MArr0 !sh {-# UNPACK #-} !(MutableByteArray s)
 
 -- | Immutable arrays of primitive type.
 
-data Arr0 sh elm = Arr0 {-# UNPACK #-} !sh {-# UNPACK #-} !ByteArray
+data Arr0 sh elm = Arr0 !sh {-# UNPACK #-} !ByteArray
 
 
 
 type instance MutArray Arr0 = MArr0
 
+-- NOTE inLb, inUb is including bound, while exUb is excluding upper bound.
+-- Differentiates between largest included index, first excluded index.
+
 instance (Shape sh, ExtShape sh, Prim elm) => MPrimArrayOps MArr0 sh elm where
-  boundsM (MArr0 ub _) = (zeroDim,ub `subDim` unitDim)
-  fromListM lb ub xs = do
-    ma <- newM lb ub
-    let ubreal = ub `addDim` unitDim
+  boundsM (MArr0 exUb _) = (zeroDim,exUb `subDim` unitDim)
+  fromListM inLb inUb xs = do
+    ma <- newM inLb inUb
+    let exUb = inUb `addDim` unitDim
     let (MArr0 _ mba) = ma
-    zipWithM_ (\k x -> assert (length xs == size ubreal) $ writeByteArray mba k x) [0.. size ubreal] xs
+    zipWithM_ (\k x -> assert (length xs == size exUb) $ writeByteArray mba k x) [0.. toIndex exUb inUb] xs
     return ma
-  newM lb ub' = let ub = ub' `addDim` unitDim in
-    unless (lb == zeroDim) (error "MArr0 lb/=zeroDim") >>
-    MArr0 ub `liftM` newByteArray (size ub * sizeOf (undefined :: elm))
-  newWithM lb ub def = do
-    let ubreal = ub `addDim` unitDim
-    ma <- newM lb ub
+  newM inLb inUb = let exUb = inUb `addDim` unitDim in
+    unless (inLb == zeroDim) (error "MArr0 lb/=zeroDim") >>
+    MArr0 exUb `liftM` newByteArray (size exUb * sizeOf (undefined :: elm))
+  newWithM inLb inUb def = do
+    let exUb = inUb `addDim` unitDim
+    ma <- newM inLb inUb
     let (MArr0 _ mba) = ma
-    forM_ [0 .. (toIndex ubreal ub)] $ \k -> writeByteArray mba k def
+    forM_ [0 .. toIndex exUb inUb] $ \k -> writeByteArray mba k def
     return ma
-  readM (MArr0 ub mba) idx = readByteArray mba (toIndex ub idx)
-  writeM (MArr0 ub mba) idx elm = writeByteArray mba (toIndex ub idx) elm
+  readM (MArr0 exUb mba) idx = assert (inShape exUb idx) $ readByteArray mba (toIndex exUb idx)
+  writeM (MArr0 exUb mba) idx elm = assert (inShape exUb idx) $ writeByteArray mba (toIndex exUb idx) elm
   {-# INLINE boundsM #-}
   {-# INLINE fromListM #-}
   {-# INLINE newM #-}
@@ -58,9 +61,9 @@ instance (Shape sh, ExtShape sh, Prim elm) => MPrimArrayOps MArr0 sh elm where
   {-# INLINE writeM #-}
 
 instance (Shape sh, ExtShape sh, Prim elm) => PrimArrayOps Arr0 sh elm where
-  bounds (Arr0 ub _) = (zeroDim,ub `subDim` unitDim)
-  freeze (MArr0 ub mba) = Arr0 ub `liftM` unsafeFreezeByteArray mba
-  index (Arr0 ub ba) idx = indexByteArray ba (toIndex ub idx)
+  bounds (Arr0 exUb _) = (zeroDim,exUb `subDim` unitDim)
+  freeze (MArr0 exUb mba) = Arr0 exUb `liftM` unsafeFreezeByteArray mba
+  index (Arr0 exUb ba) idx = assert (inShape exUb idx) $ indexByteArray ba (toIndex exUb idx)
   {-# INLINE bounds #-}
   {-# INLINE freeze #-}
   {-# INLINE index #-}
