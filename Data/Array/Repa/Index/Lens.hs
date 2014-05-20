e7d315945b75712f19df9c92396ebbac259d7474
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2014-05-20 13:13:12 +0200

    re-activated lens stuff
    upgrading all dependent libraries

--- Data/Array/Repa/Index/Lens.hs
+++ Data/Array/Repa/Index/Lens.hs
@@ -8,8 +8,6 @@
 
 module Data.Array.Repa.Index.Lens where
 
-{-
-
 import           Control.Applicative
 import           Control.Lens
 import           Data.Array.Repa.Index
@@ -123,6 +121,4 @@ instance Field6 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c:.d:.e':.f) e e' where
 instance Field7 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c:.d:.e:.f') f f' where
   _7 k ~(Z:.a:.b:.c:.d:.e:.f) = k f <&> \f' -> (Z:.a:.b:.c:.d:.e:.f')
   {-# INLINE _7 #-}
-
--}
 
