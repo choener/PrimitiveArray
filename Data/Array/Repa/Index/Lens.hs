5e88b936c2055e15ef432619be7e13c3a6602529
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2014-05-29 19:40:00 +0200

    renamed the lenses such das (Z:.a), a is _1

--- Data/Array/Repa/Index/Lens.hs
+++ Data/Array/Repa/Index/Lens.hs
@@ -21,104 +21,99 @@ class IndexLens i where
 
 
 
--- * Make @(Z:.a:.b ...)@ isomorphic to @(_,a,b,...)@ with @Field1@ assumed to
--- be @Z@ (but not actually instanced)
---
--- TODO use TH for this?
---
--- TODO currently we have 6 dim + @Z@. Do we need 8 dim?
+-- ** @Field@ instances for inductive tuples.
 
-instance Field2 (Z:.a) (Z:.a') a a' where
-  _2 k ~(Z:.a) = k a <&> \a' -> (Z:.a')
-  {-# INLINE _2 #-}
+instance Field1 (Z:.a) (Z:.a') a a' where
+  _1 k ~(Z:.a) = k a <&> \a' -> (Z:.a')
+  {-# INLINE _1 #-}
 
-instance Field2 (Z:.a:.b) (Z:.a':.b) a a' where
-  _2 k ~(Z:.a:.b) = k a <&> \a' -> (Z:.a':.b)
-  {-# INLINE _2 #-}
+instance Field1 (Z:.a:.b) (Z:.a':.b) a a' where
+  _1 k ~(Z:.a:.b) = k a <&> \a' -> (Z:.a':.b)
+  {-# INLINE _1 #-}
 
-instance Field2 (Z:.a:.b:.c) (Z:.a':.b:.c) a a' where
-  _2 k ~(Z:.a:.b:.c) = k a <&> \a' -> (Z:.a':.b:.c)
-  {-# INLINE _2 #-}
+instance Field1 (Z:.a:.b:.c) (Z:.a':.b:.c) a a' where
+  _1 k ~(Z:.a:.b:.c) = k a <&> \a' -> (Z:.a':.b:.c)
+  {-# INLINE _1 #-}
 
-instance Field2 (Z:.a:.b:.c:.d) (Z:.a':.b:.c:.d) a a' where
-  _2 k ~(Z:.a:.b:.c:.d) = k a <&> \a' -> (Z:.a':.b:.c:.d)
-  {-# INLINE _2 #-}
+instance Field1 (Z:.a:.b:.c:.d) (Z:.a':.b:.c:.d) a a' where
+  _1 k ~(Z:.a:.b:.c:.d) = k a <&> \a' -> (Z:.a':.b:.c:.d)
+  {-# INLINE _1 #-}
 
-instance Field2 (Z:.a:.b:.c:.d:.e) (Z:.a':.b:.c:.d:.e) a a' where
-  _2 k ~(Z:.a:.b:.c:.d:.e) = k a <&> \a' -> (Z:.a':.b:.c:.d:.e)
-  {-# INLINE _2 #-}
+instance Field1 (Z:.a:.b:.c:.d:.e) (Z:.a':.b:.c:.d:.e) a a' where
+  _1 k ~(Z:.a:.b:.c:.d:.e) = k a <&> \a' -> (Z:.a':.b:.c:.d:.e)
+  {-# INLINE _1 #-}
 
-instance Field2 (Z:.a:.b:.c:.d:.e:.f) (Z:.a':.b:.c:.d:.e:.f) a a' where
-  _2 k ~(Z:.a:.b:.c:.d:.e:.f) = k a <&> \a' -> (Z:.a':.b:.c:.d:.e:.f)
-  {-# INLINE _2 #-}
+instance Field1 (Z:.a:.b:.c:.d:.e:.f) (Z:.a':.b:.c:.d:.e:.f) a a' where
+  _1 k ~(Z:.a:.b:.c:.d:.e:.f) = k a <&> \a' -> (Z:.a':.b:.c:.d:.e:.f)
+  {-# INLINE _1 #-}
 
 --
 
-instance Field3 (Z:.a:.b) (Z:.a:.b') b b' where
-  _3 k ~(Z:.a:.b) = k b <&> \b' -> (Z:.a:.b')
+instance Field2 (Z:.a:.b) (Z:.a:.b') b b' where
+  _2 k ~(Z:.a:.b) = k b <&> \b' -> (Z:.a:.b')
+  {-# INLINE _2 #-}
+
+instance Field2 (Z:.a:.b:.c) (Z:.a:.b':.c) b b' where
+  _2 k ~(Z:.a:.b:.c) = k b <&> \b' -> (Z:.a:.b':.c)
+  {-# INLINE _2 #-}
+
+instance Field2 (Z:.a:.b:.c:.d) (Z:.a:.b':.c:.d) b b' where
+  _2 k ~(Z:.a:.b:.c:.d) = k b <&> \b' -> (Z:.a:.b':.c:.d)
+  {-# INLINE _2 #-}
+
+instance Field2 (Z:.a:.b:.c:.d:.e) (Z:.a:.b':.c:.d:.e) b b' where
+  _2 k ~(Z:.a:.b:.c:.d:.e) = k b <&> \b' -> (Z:.a:.b':.c:.d:.e)
+  {-# INLINE _2 #-}
+
+instance Field2 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b':.c:.d:.e:.f) b b' where
+  _2 k ~(Z:.a:.b:.c:.d:.e:.f) = k b <&> \b' -> (Z:.a:.b':.c:.d:.e:.f)
+  {-# INLINE _2 #-}
+
+--
+
+instance Field3 (Z:.a:.b:.c) (Z:.a:.b:.c') c c' where
+  _3 k ~(Z:.a:.b:.c) = k c <&> \c' -> (Z:.a:.b:.c')
   {-# INLINE _3 #-}
 
-instance Field3 (Z:.a:.b:.c) (Z:.a:.b':.c) b b' where
-  _3 k ~(Z:.a:.b:.c) = k b <&> \b' -> (Z:.a:.b':.c)
+instance Field3 (Z:.a:.b:.c:.d) (Z:.a:.b:.c':.d) c c' where
+  _3 k ~(Z:.a:.b:.c:.d) = k c <&> \c' -> (Z:.a:.b:.c':.d)
   {-# INLINE _3 #-}
 
-instance Field3 (Z:.a:.b:.c:.d) (Z:.a:.b':.c:.d) b b' where
-  _3 k ~(Z:.a:.b:.c:.d) = k b <&> \b' -> (Z:.a:.b':.c:.d)
+instance Field3 (Z:.a:.b:.c:.d:.e) (Z:.a:.b:.c':.d:.e) c c' where
+  _3 k ~(Z:.a:.b:.c:.d:.e) = k c <&> \c' -> (Z:.a:.b:.c':.d:.e)
   {-# INLINE _3 #-}
 
-instance Field3 (Z:.a:.b:.c:.d:.e) (Z:.a:.b':.c:.d:.e) b b' where
-  _3 k ~(Z:.a:.b:.c:.d:.e) = k b <&> \b' -> (Z:.a:.b':.c:.d:.e)
-  {-# INLINE _3 #-}
-
-instance Field3 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b':.c:.d:.e:.f) b b' where
-  _3 k ~(Z:.a:.b:.c:.d:.e:.f) = k b <&> \b' -> (Z:.a:.b':.c:.d:.e:.f)
+instance Field3 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c':.d:.e:.f) c c' where
+  _3 k ~(Z:.a:.b:.c:.d:.e:.f) = k c <&> \c' -> (Z:.a:.b:.c':.d:.e:.f)
   {-# INLINE _3 #-}
 
 --
 
-instance Field4 (Z:.a:.b:.c) (Z:.a:.b:.c') c c' where
-  _4 k ~(Z:.a:.b:.c) = k c <&> \c' -> (Z:.a:.b:.c')
+instance Field4 (Z:.a:.b:.c:.d) (Z:.a:.b:.c:.d') d d' where
+  _4 k ~(Z:.a:.b:.c:.d) = k d <&> \d' -> (Z:.a:.b:.c:.d')
   {-# INLINE _4 #-}
 
-instance Field4 (Z:.a:.b:.c:.d) (Z:.a:.b:.c':.d) c c' where
-  _4 k ~(Z:.a:.b:.c:.d) = k c <&> \c' -> (Z:.a:.b:.c':.d)
+instance Field4 (Z:.a:.b:.c:.d:.e) (Z:.a:.b:.c:.d':.e) d d' where
+  _4 k ~(Z:.a:.b:.c:.d:.e) = k d <&> \d' -> (Z:.a:.b:.c:.d':.e)
   {-# INLINE _4 #-}
 
-instance Field4 (Z:.a:.b:.c:.d:.e) (Z:.a:.b:.c':.d:.e) c c' where
-  _4 k ~(Z:.a:.b:.c:.d:.e) = k c <&> \c' -> (Z:.a:.b:.c':.d:.e)
-  {-# INLINE _4 #-}
-
-instance Field4 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c':.d:.e:.f) c c' where
-  _4 k ~(Z:.a:.b:.c:.d:.e:.f) = k c <&> \c' -> (Z:.a:.b:.c':.d:.e:.f)
+instance Field4 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c:.d':.e:.f) d d' where
+  _4 k ~(Z:.a:.b:.c:.d:.e:.f) = k d <&> \d' -> (Z:.a:.b:.c:.d':.e:.f)
   {-# INLINE _4 #-}
 
 --
 
-instance Field5 (Z:.a:.b:.c:.d) (Z:.a:.b:.c:.d') d d' where
-  _5 k ~(Z:.a:.b:.c:.d) = k d <&> \d' -> (Z:.a:.b:.c:.d')
+instance Field5 (Z:.a:.b:.c:.d:.e) (Z:.a:.b:.c:.d:.e') e e' where
+  _5 k ~(Z:.a:.b:.c:.d:.e) = k e <&> \e' -> (Z:.a:.b:.c:.d:.e')
   {-# INLINE _5 #-}
 
-instance Field5 (Z:.a:.b:.c:.d:.e) (Z:.a:.b:.c:.d':.e) d d' where
-  _5 k ~(Z:.a:.b:.c:.d:.e) = k d <&> \d' -> (Z:.a:.b:.c:.d':.e)
-  {-# INLINE _5 #-}
-
-instance Field5 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c:.d':.e:.f) d d' where
-  _5 k ~(Z:.a:.b:.c:.d:.e:.f) = k d <&> \d' -> (Z:.a:.b:.c:.d':.e:.f)
+instance Field5 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c:.d:.e':.f) e e' where
+  _5 k ~(Z:.a:.b:.c:.d:.e:.f) = k e <&> \e' -> (Z:.a:.b:.c:.d:.e':.f)
   {-# INLINE _5 #-}
 
 --
 
-instance Field6 (Z:.a:.b:.c:.d:.e) (Z:.a:.b:.c:.d:.e') e e' where
-  _6 k ~(Z:.a:.b:.c:.d:.e) = k e <&> \e' -> (Z:.a:.b:.c:.d:.e')
+instance Field6 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c:.d:.e:.f') f f' where
+  _6 k ~(Z:.a:.b:.c:.d:.e:.f) = k f <&> \f' -> (Z:.a:.b:.c:.d:.e:.f')
   {-# INLINE _6 #-}
-
-instance Field6 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c:.d:.e':.f) e e' where
-  _6 k ~(Z:.a:.b:.c:.d:.e:.f) = k e <&> \e' -> (Z:.a:.b:.c:.d:.e':.f)
-  {-# INLINE _6 #-}
-
---
-
-instance Field7 (Z:.a:.b:.c:.d:.e:.f) (Z:.a:.b:.c:.d:.e:.f') f f' where
-  _7 k ~(Z:.a:.b:.c:.d:.e:.f) = k f <&> \f' -> (Z:.a:.b:.c:.d:.e:.f')
-  {-# INLINE _7 #-}
 
