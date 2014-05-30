8952a427719932be089d104feff14265896563a7
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2014-05-31 01:53:05 +0200

    updated the generic table-filling algorithm

--- Data/PrimitiveArray/FillTables.hs
+++ Data/PrimitiveArray/FillTables.hs
@@ -3,11 +3,12 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
 
--- | Operations to fill primitive arrays. Arrays are combined just like indices
--- using 'Z' and '(:.)'. This allows filling an unlimited number of tables.
---
--- TODO make explicit in which order the tables are filled.
+-- | Operations to fill primitive arrays. Arrays are combined just like
+-- indices using 'Z' and '(:.)'. This allows filling an unlimited number of
+-- tables.
 
 module Data.PrimitiveArray.FillTables where
 
@@ -15,89 +16,73 @@ import Control.Monad.Primitive
 import Data.Array.Repa.Index
 import Data.Array.Repa.Shape
 import Data.Vector.Fusion.Stream.Monadic as S
+import Data.Vector.Fusion.Stream.Size
+import Control.Monad (when)
 
 import Data.PrimitiveArray.Class
 import Data.Array.Repa.Index.Subword
+import Data.Array.Repa.Index.Points
+import Data.PrimitiveArray.Zero
 
 
 
--- * Driver classes for table filling system.
+-- * High-level table filling system.
 
--- Upper triangular table filling. Right now, only a serial option 'upperTriS'
--- is available.
+-- | Run the forward phase of algorithms.
+
+runFillTables (ts:.t) = S.mapM_ (unsafeWriteCell (ts:.t)) $ fillTables from to where
+  (from,to) = boundsM t
+{-# INLINE runFillTables #-}
+
+-- | Captures creating the indices filling tables in the correct order. We
+-- assume that it does *not* matter which of the tables is filled first. If
+-- you need to fill tables in a sequential order, bind to s.th. like
+-- @(Z:.s:.t)@ and then run @runFillTables (Z:.s) >> runFillTables (Z:.t)@.
+
+class FillTables sh where
+    fillTables :: Monad m => sh -> sh -> S.Stream m sh
+
+instance FillTables Z where
+    fillTables _ _ = S.singleton Z
+    {-# INLINE fillTables #-}
+
+-- | Create index stream with an active @PointL 0 m@. Note that we
+-- currently assume that the first index is indeed 0.
 --
--- TODO Using Repa, 'upperTriP' will soon become available.
+-- TODO Rewrite to use the @HERMIT in the stream@ trick of stopping at 0.
+-- Will require some changes to @step@. (We can't just do @[t,t-1,..f]@ as
+-- we have the dependency that @k@ requires @k-1@.
 
-class UpperTriS m stack where
-  upperTriS :: stack -> m ()
-
--- | Defines how a single index in a stack of arrays + evaluation functions is
--- handled. The instances *should* work for any index @ix@.
-
-class Stack m sh xs where
-  writeStack :: xs -> sh -> m ()
-
-
-
--- * Instances
-
--- ** 1-tape grammars with 'Subword' indices.
-
--- TODO Insert check that all extends are the same!
-
-instance
-  ( Monad m
-  , MPrimArrayOps arr (Z:.Subword) e
-  , Stack m Subword (xs :. SubwordNonTerminal m arr e)
-  ) => UpperTriS m  (xs :. SubwordNonTerminal m arr e) where
-  upperTriS xs@(_:.(x,f)) = do
-    -- TODO missing extends check
-    let (Z:.Subword (l:._),Z:.Subword (u:._)) = boundsM x
-    S.mapM_ (go xs) $ unfolder l u
-    where
-      -- Write all table values at a certain subword. Note that tables are
-      -- filled left to right in the stack order '(Z:.first:.next:.last)'
-      go xs (i,j) = writeStack xs (Subword (i:.j))
-      {-# INLINE go #-}
-      -- the unfolder steps through the diagonals from the main toward the
-      -- upper-right. It starts a the main-diagonal column and goes to the
-      -- right in each big step '(i==u)' and increases the row by one in each
-      -- small step.
-      unfolder l u = S.unfoldr step (l,l) where
-        step (j,i)
-          | j> u      = Nothing
-          | i==u      = Just ((i,j),(j+1,l))
-          | otherwise = Just ((i,j),(j,i+1))
-        {-# INLINE step #-}
-      {-# INLINE unfolder #-}
-  {-# INLINE upperTriS #-}
-
-instance
-  ( PrimMonad m
-  , Stack m Subword xs
-  , MPrimArrayOps arr (Z:.Subword) e
-  ) => Stack m Subword (xs :. SubwordNonTerminal m arr e) where
-  writeStack (xs:.(x,f)) i = writeStack xs i >> f i >>= writeM x (Z:.i)
-  {-# INLINE writeStack #-}
-
--- ** Multi-tape indices.
-
-instance (Monad m) => Stack m sh Z where
-  writeStack _ _ = return ()
-  {-# INLINE writeStack #-}
-
-instance
-  ( PrimMonad m
-  , Stack m ix xs
-  , MPrimArrayOps arr ix e
-  ) => Stack m ix (xs :. GeneralNonTerminal m arr ix e) where
-  writeStack (xs:.(x,f)) i = writeStack xs i >> f i >>= writeM x i
-  {-# INLINE writeStack #-}
+instance FillTables is => FillTables (is:.PointL) where
+    fillTables (fs:.PointL (0:.f)) (ts:.PointL (0:.t)) = S.flatten mk step Unknown $ fillTables fs ts where
+      mk is = return (is:.f)
+      step (is:.k)
+        | k>t       = return $ S.Done
+        | otherwise = return $ S.Yield (is:.pointL 0 k) (is:.(k+1))
+      {-# INLINE [1] mk #-}
+      {-# INLINE [1] step #-}
+    {-# INLINE fillTables #-}
 
 
 
--- Wrap non-terminal symbol type, corresponding rule type.
+-- * Write to individuel cells.
 
-type SubwordNonTerminal m arr e = (MutArr m (arr (Z:.Subword) e), Subword -> m e)
-type GeneralNonTerminal m arr ix e = (MutArr m (arr ix e), ix -> m e)
+-- | 'WriteCell' provides methods to fill all cells with a specific index
+-- @sh@ in a stack of non-terminal tables @c@.
+
+class WriteCell m c sh where
+    unsafeWriteCell :: c -> sh -> m ()
+    writeCell :: c -> sh -> m ()
+
+instance (Monad m) => WriteCell m Z sh where
+    unsafeWriteCell _ _ = return ()
+    writeCell _ _ = return ()
+    {-# INLINE unsafeWriteCell #-}
+    {-# INLINE writeCell #-}
+
+instance (f ~ (sh -> m a), t ~ MutArr m (arr sh a), WriteCell m cs sh, Monad m, MPrimArrayOps arr sh a, PrimMonad m) => WriteCell m (cs:.(t,f)) sh where
+    unsafeWriteCell (cs:.(t,f)) sh = unsafeWriteCell cs sh >> f sh >>= writeM t sh
+    writeCell (cs:.(t,f)) sh = writeCell cs sh >> when (inBoundsM t sh) (f sh >>= writeM t sh)
+    {-# INLINE unsafeWriteCell #-}
+    {-# INLINE writeCell #-}
 
