566a07904f33f390ea9f3b58afe4674601de4c55
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2014-08-14 04:26:40 +0200

    implemented a nice rangeStream for PathSet
    should stream nicely and be quite fast

--- Data/Array/Repa/Index/Set.hs
+++ Data/Array/Repa/Index/Set.hs
@@ -12,17 +12,21 @@
 
 module Data.Array.Repa.Index.Set where
 
-import Data.Aeson
-import Data.Array.Repa.Index
-import Data.Array.Repa.Shape
-import Data.Binary
-import Data.Bits
-import Data.Serialize
-import Data.Vector.Unboxed.Deriving
-import GHC.Generics
+import           Data.Aeson
+import           Data.Array.Repa.Index
+import           Data.Array.Repa.Shape
+import           Data.Binary
+import           Data.Bits
+import           Data.Serialize
+import           Data.Vector.Fusion.Stream.Size
+import           Data.Vector.Unboxed.Deriving
+import           GHC.Generics
+import qualified Data.Vector.Fusion.Stream.Monadic as M
+import qualified Data.Vector.Generic as G
+import qualified Data.Vector.Unboxed as VU
 
-import Data.Bits.Ordered
-import Data.Array.Repa.ExtShape
+import           Data.Bits.Ordered
+import           Data.Array.Repa.ExtShape
 
 
 
@@ -66,14 +70,20 @@ instance Shape sh => Shape (sh:.PathSet) where
   {-# INLINE [1] zeroDim #-}
   zeroDim = zeroDim :. PathSet 0 0 0
   {-# INLINE [1] unitDim #-}
-  unitDim = unitDim :. PathSet 1 0 0
+  -- TODO do we need 1/1/1 or 1/0/0 ? pretty sure about 1 1 1 but I am
+  -- using popcounts in size/toIndex currently anyway (which I shouldn't!)
+  -- but at least the "pointed to element" stays the same as it is shifted
+  -- as well (which probably means I should use @rs `shiftR` popCount ls
+  -- + ls@
+  unitDim = unitDim :. PathSet 1 1 1
   {-# INLINE [1] intersectDim #-}
   intersectDim = error "sh:.PathSet / intersectDim"
   {-# INLINE [1] addDim #-}
-  addDim = error "do not use addDim for the PrimitiveArray stuff"
+  addDim (sh1 :. PathSet ls lf ll) (sh2 :. PathSet rs rf rl)
+    = addDim sh1 sh2 :. PathSet (ls `shiftL` popCount rs) (lf+rf) (ll+rl)
   {-# INLINE [1] size #-}
   size (sh1 :. PathSet s _ _)
-    = let !p = popCount s in size (sh1:.s+1:.p:.p)
+    = let !p = popCount s in size (sh1:.s:.p:.p)
     -- let p = popCount s + 1 in size sh1 * s * p * p
   {-# INLINE [1] sizeIsValid #-}
   sizeIsValid (sh1 :. PathSet p _ _)
@@ -101,12 +111,37 @@ instance Shape sh => Shape (sh:.PathSet) where
   {-# INLINE deepSeq #-}
   deepSeq (sh :. n) x = deepSeq sh (n `seq` x)
 
-{-
 instance ExtShape sh => ExtShape (sh:.PathSet) where
   {-# INLINE [1] subDim #-}
-  -- TODO check this!
-  subDim (sh1:.PathSet s _ _) (sh2:.PathSet t _ _)
-    = let p = popCount (s-t)
-      in  subDim sh1 sh2 :. PathSet (s-t) p p
--}
+  subDim (sh1 :. PathSet ls lf ll) (sh2 :. PathSet rs rf rl)
+    = subDim sh1 sh2 :. PathSet (ls `shiftR` popCount rs) (lf-rf) (ll-rl)
+  {-# INLINE rangeList #-}
+  rangeList _ _ = error "rangeList/not implemented"
+  {-# INLINE rangeStream #-}
+  -- we assume a set starting at "nothing set"
+  rangeStream (sh1 :. PathSet _ _ _) (sh2 :. PathSet rs rf rl) = M.flatten mk step Unknown $ rangeStream sh1 sh2
+    where mk is = let v = popCntMemoInt (popCount rs)
+                  in  return $ Left (is:.v)
+          step (Left (is:.v))
+            | G.null v  = return $ M.Done
+            | pcnt == 0 = return $ M.Yield (is:.PathSet  0 0 0) $ Left  (is:.vt) -- only one case
+            | pcnt == 1 = return $ M.Yield (is:.PathSet vh l l) $ Left  (is:.vt) -- only one case
+            | otherwise = return $ M.Skip                       $ Right (is:.v:.vh:.vh `clearBit` l)  -- prepare bit pools
+            where pcnt = popCount  vh
+                  l    = lsbActive vh
+                  vh   = G.unsafeHead v
+                  vt   = G.unsafeTail v
+          step (Right (is:.v:.fp:.lp))
+            | fp==0     = return $ M.Skip                         $ Left  (is:.vt)  -- the fp pool is empty, reset everything
+            | lp==0     = return $ M.Skip                         $ Right (is:.v:.fn:.vh `clearBit` lsbActive fn) -- new lp pool with cutten fp
+            | otherwise = return $ M.Yield (is:.PathSet vh af al) $ Right (is:.v:.fp:.lp `clearBit` al) -- just continue with next lp pool element
+            where vh = G.unsafeHead v
+                  vt = G.unsafeTail v
+                  af = lsbActive fp
+                  al = lsbActive lp
+                  fn = fp `clearBit` af
+          {-# INLINE [1] mk #-}
+          {-# INLINE [1] step #-}
+  {-# INLINE topmostIndex #-}
+  topmostIndex _ _ = error "topmostIndex/not implemented"
 
