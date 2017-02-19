85bb654df508fd92b56eb5b19c59c129c41587ea
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-02-19 21:33:41 +0100

    added row score information

--- Data/PrimitiveArray/ScoreMatrix.hs
+++ Data/PrimitiveArray/ScoreMatrix.hs
@@ -23,6 +23,7 @@ import qualified Data.PrimitiveArray as PA
 
 data ScoreMatrix t = ScoreMatrix
   { scoreMatrix :: !(Unboxed (Z:.Int:.Int) t)
+  , scoreNodes  :: !(Unboxed Int t)
   , rowNames    :: !(V.Vector Text)
   , colNames    :: !(V.Vector Text)
   } deriving (Eq,Show)
@@ -30,44 +31,51 @@ data ScoreMatrix t = ScoreMatrix
 -- | Get the distance between edges @(From,To)@.
 
 (.!.) :: Unbox t => ScoreMatrix t -> (Int,Int) -> t
-ScoreMatrix mat _ _ .!. (f,t) = mat ! (Z:.f:.t)
+ScoreMatrix mat _ _ _ .!. (f,t) = mat ! (Z:.f:.t)
 {-# Inline (.!.) #-}
+
+-- | If the initial node has a "distance", it'll be here
+
+nodeDist :: Unbox t => ScoreMatrix t -> Int -> t
+nodeDist ScoreMatrix{..} k = scoreNodes ! k
 
 -- | Get the name of the node at an row index
 
 rowNameOf :: ScoreMatrix t -> Int -> Text
-rowNameOf (ScoreMatrix _ rs cs) k = rs V.! k
+rowNameOf ScoreMatrix{..} k = rowNames V.! k
 {-# Inline rowNameOf #-}
 
 -- | Get the name of the node at an column index
 
 colNameOf :: ScoreMatrix t -> Int -> Text
-colNameOf (ScoreMatrix _ rs cs) k = cs V.! k
+colNameOf ScoreMatrix{..} k = colNames V.! k
 {-# Inline colNameOf #-}
 
 -- | Number of rows in a score matrix.
 
 numRows :: Unbox t => ScoreMatrix t -> Int
-numRows (ScoreMatrix mat _ _) = let ((Z:.0:.0),(Z:.n':._)) = bounds mat in n' + 1
+numRows ScoreMatrix{..} = let ((Z:.0:.0),(Z:.n':._)) = bounds scoreMatrix in n' + 1
 {-# Inline numRows #-}
 
 -- | Number of columns in a score matrix.
 
 numCols :: Unbox t => ScoreMatrix t -> Int
-numCols (ScoreMatrix mat _ _) = let ((Z:.0:.0),(Z:._:.n')) = bounds mat in n' + 1
+numCols ScoreMatrix{..} = let ((Z:.0:.0),(Z:._:.n')) = bounds scoreMatrix in n' + 1
 {-# Inline numCols #-}
 
 listOfRowNames :: ScoreMatrix t -> [Text]
-listOfRowNames (ScoreMatrix _ ns _) = V.toList ns
+listOfRowNames ScoreMatrix{..} = V.toList rowNames
 
 listOfColNames :: ScoreMatrix t -> [Text]
-listOfColNames (ScoreMatrix _ _ ns) = V.toList ns
+listOfColNames ScoreMatrix{..} = V.toList colNames
 
 -- | Turns a @ScoreMatrix@ for distances into one scaled by "temperature" for
 -- Inside/Outside calculations. Each value is scaled by
 -- @\k -> exp $ negate k / r * t@ where
 -- r = (n-1) * d
 -- d = mean of genetic distance
+--
+-- Node scores are turned directly into probabilities.
 --
 -- TODO Again, there is overlap and we should really have @newtype
 -- Distance@ and friends.
@@ -81,8 +89,9 @@ toPartMatrix
   -- ^ temperature
   -> ScoreMatrix Double
   -> ScoreMatrix (Log Double)
-toPartMatrix t scoreMat@(ScoreMatrix mat rns cns) = ScoreMatrix p rns cns
+toPartMatrix t scoreMat@(ScoreMatrix mat sn rns cns) = ScoreMatrix p psn rns cns
   where p = PA.map (\k -> Exp {- . log . exp -} $ negate k / (r * t)) mat
+        psn = PA.map (\k -> Exp $ negate k) sn
         n = numRows scoreMat
         d = Prelude.sum [ mat ! (Z:.i:.j) | i <- [0..n-1], j <- [i+1..n-1] ] / fromIntegral (n*(n-1))
         r = fromIntegral (n-1) * d
@@ -104,10 +113,11 @@ fromFile fp = do
     putStrLn $ fp ++ " is not a NxN matrix"
     print mat'
     exitFailure
-  let mat = PA.fromAssocs (Z:.0:.0) (Z:.n-1:.n-1) 0
+  let scoreMatrix = PA.fromAssocs (Z:.0:.0) (Z:.n-1:.n-1) 0
           $ concatMap (\(r,es) -> [ ((Z:.r:.c),e) | (c,e) <- zip [0..] es ])
           $ zip [0..] mat' -- rows
+  let scoreNodes = PA.fromAssocs 0 (n-1) 0 []
   let rowNames = V.fromList . map T.pack . drop 1 . words $ head ls
   let colNames = V.fromList . map (T.pack . head . words) $ tail ls
-  return $ ScoreMatrix mat rowNames colNames
+  return $ ScoreMatrix{..} -- mat rowNames colNames (V.fromList $ replicate n 0)
 
