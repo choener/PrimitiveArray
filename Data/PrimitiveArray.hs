99b4a325268dc7fd3f19be95f8ce42e273bc297a
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-12-07 22:08:22 +0100

    more cleanup of modules

--- Data/PrimitiveArray.hs
+++ Data/PrimitiveArray.hs
@@ -2,12 +2,12 @@
 module Data.PrimitiveArray 
   ( module Data.PrimitiveArray.Class
   , module Data.PrimitiveArray.Dense
-  , module Data.PrimitiveArray.FillTables
+--  , module Data.PrimitiveArray.FillTables
   , module Data.PrimitiveArray.Index
   ) where
 
 import Data.PrimitiveArray.Class
 import Data.PrimitiveArray.Dense
-import Data.PrimitiveArray.FillTables
+--import Data.PrimitiveArray.FillTables
 import Data.PrimitiveArray.Index
 
--- Data/PrimitiveArray/Index.hs
+++ Data/PrimitiveArray/Index.hs
@@ -6,7 +6,7 @@ module Data.PrimitiveArray.Index
 --  , module Data.PrimitiveArray.Index.PhantomInt
   , module Data.PrimitiveArray.Index.Point
 --  , module Data.PrimitiveArray.Index.Set
---  , module Data.PrimitiveArray.Index.Subword
+  , module Data.PrimitiveArray.Index.Subword
   , module Data.PrimitiveArray.Index.Unit
   ) where
 
@@ -17,6 +17,6 @@ import Data.PrimitiveArray.Index.IOC
 --import Data.PrimitiveArray.Index.PhantomInt hiding (streamUpMk, streamUpStep, streamDownMk, streamDownStep)
 import Data.PrimitiveArray.Index.Point hiding (streamUpMk, streamUpStep, streamDownMk, streamDownStep)
 --import Data.PrimitiveArray.Index.Set hiding (streamUpBsMk, streamUpBsStep, streamDownBsMk, StreamDownBsStep, streamUpBsIMk, streamUpBsIStep, streamDownBsIMk, StreamDownBsIStep, streamUpBsIiMk, streamUpBsIiStep, streamDownBsIiMk, StreamDownBsIiStep)
---import Data.PrimitiveArray.Index.Subword hiding (streamUpMk, streamUpStep, streamDownMk, streamDownStep)
+import Data.PrimitiveArray.Index.Subword hiding (streamUpMk, streamUpStep, streamDownMk, streamDownStep)
 import Data.PrimitiveArray.Index.Unit
 
