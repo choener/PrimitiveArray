bfb3e1cfb815079e2585c956afd2e4daedaee038
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-02-16 01:43:25 +0100

    phantom ints are back

--- Data/PrimitiveArray/Index/PhantomInt.hs
+++ Data/PrimitiveArray/Index/PhantomInt.hs
@@ -11,14 +11,13 @@ import Data.Hashable (Hashable)
 import Data.Ix(Ix)
 import Data.Serialize (Serialize)
 import Data.Typeable
-import Data.Vector.Fusion.Stream.Monadic (map,Step(..))
+import Data.Vector.Fusion.Stream.Monadic (map,Step(..),flatten)
 import Data.Vector.Unboxed.Deriving
 import GHC.Generics (Generic)
 import Prelude hiding (map)
 
 import Data.PrimitiveArray.Index.Class
 import Data.PrimitiveArray.Index.IOC
-import Data.PrimitiveArray.Vector.Compat
 
 
 
@@ -26,7 +25,7 @@ import Data.PrimitiveArray.Vector.Compat
 -- type @p@. In particular, the @Index@ and @IndexStream@ instances are the
 -- same as for raw @Int@s.
 
-newtype PInt (t ∷ k) (p ∷ k) = PInt { getPInt :: Int }
+newtype PInt (ioc ∷ k) (p ∷ k) = PInt { getPInt :: Int }
   deriving (Read,Show,Eq,Ord,Enum,Num,Integral,Real,Generic,Data,Typeable,Ix)
 
 pIntI :: Int -> PInt I p
@@ -54,38 +53,44 @@ instance Hashable     (PInt t p)
 instance NFData       (PInt t p)
 
 instance Index (PInt t p) where
-  type LimitType (PInt t p) = Int
+  newtype LimitType (PInt t p) = LtPInt Int
   linearIndex _ (PInt k) = k
   {-# Inline linearIndex #-}
-  size _ h = h+1
+  size (LtPInt h) = h+1
   {-# Inline size #-}
-  inBounds h (PInt k) = 0 <= k && k <= h
+  inBounds (LtPInt h) (PInt k) = 0 <= k && k <= h
   {-# Inline inBounds #-}
 
 instance IndexStream z => IndexStream (z:.PInt I p) where
-  streamUp   (ls:.l) (hs:.h) = flatten (streamUpMk   l h) (streamUpStep   l h) $ streamUp ls hs
-  streamDown (ls:.l) (hs:.h) = flatten (streamDownMk l h) (streamDownStep l h) $ streamDown ls hs
+  streamUp   (ls:..LtPInt l) (hs:..LtPInt h) = flatten (streamUpMk   l h) (streamUpStep   l h) $ streamUp ls hs
+  streamDown (ls:..LtPInt l) (hs:..LtPInt h) = flatten (streamDownMk l h) (streamDownStep l h) $ streamDown ls hs
   {-# Inline streamUp   #-}
   {-# Inline streamDown #-}
 
 instance IndexStream z => IndexStream (z:.PInt O p) where
-  streamUp   (ls:.l) (hs:.h) = flatten (streamDownMk l h) (streamDownStep l h) $ streamUp ls hs
-  streamDown (ls:.l) (hs:.h) = flatten (streamUpMk   l h) (streamUpStep   l h) $ streamDown ls hs
+  streamUp   (ls:..LtPInt l) (hs:..LtPInt h) = flatten (streamDownMk l h) (streamDownStep l h) $ streamUp ls hs
+  streamDown (ls:..LtPInt l) (hs:..LtPInt h) = flatten (streamUpMk   l h) (streamUpStep   l h) $ streamDown ls hs
   {-# Inline streamUp   #-}
   {-# Inline streamDown #-}
 
 instance IndexStream z => IndexStream (z:.PInt C p) where
-  streamUp   (ls:.l) (hs:.h) = flatten (streamUpMk   l h) (streamUpStep   l h) $ streamUp ls hs
-  streamDown (ls:.l) (hs:.h) = flatten (streamDownMk l h) (streamDownStep l h) $ streamDown ls hs
+  streamUp   (ls:..LtPInt l) (hs:..LtPInt h) = flatten (streamUpMk   l h) (streamUpStep   l h) $ streamUp ls hs
+  streamDown (ls:..LtPInt l) (hs:..LtPInt h) = flatten (streamDownMk l h) (streamDownStep l h) $ streamDown ls hs
   {-# Inline streamUp   #-}
   {-# Inline streamDown #-}
+
+instance IndexStream (Z:.PInt ioc p) => IndexStream (PInt ioc p) where
+  streamUp l h = map (\(Z:.i) -> i) $ streamUp (ZZ:..l) (ZZ:..h)
+  {-# INLINE streamUp #-}
+  streamDown l h = map (\(Z:.i) -> i) $ streamDown (ZZ:..l) (ZZ:..h)
+  {-# INLINE streamDown #-}
 
 streamUpMk l h z = return (z,l)
 {-# Inline [0] streamUpMk #-}
 
 streamUpStep l h (z,k)
   | k > h     = return $ Done
-  | otherwise = return $ Yield (z:.k) (z,k+1)
+  | otherwise = return $ Yield (z:.PInt k) (z,k+1)
 {-# Inline [0] streamUpStep #-}
 
 streamDownMk l h z = return (z,h)
@@ -93,50 +98,6 @@ streamDownMk l h z = return (z,h)
 
 streamDownStep l h (z,k)
   | k < l     = return $ Done
-  | otherwise = return $ Yield (z:.k) (z,k-1)
+  | otherwise = return $ Yield (z:.PInt k) (z,k-1)
 {-# Inline [0] streamDownStep #-}
-
-instance IndexStream (PInt I p) where
-  streamUp l h = map (\(Z:.i) -> i) $ streamUp (Z:.l) (Z:.h)
-  {-# INLINE streamUp #-}
-  streamDown l h = map (\(Z:.i) -> i) $ streamDown (Z:.l) (Z:.h)
-  {-# INLINE streamDown #-}
-
-instance IndexStream (PInt O p) where
-  streamUp l h = map (\(Z:.i) -> i) $ streamUp (Z:.l) (Z:.h)
-  {-# INLINE streamUp #-}
-  streamDown l h = map (\(Z:.i) -> i) $ streamDown (Z:.l) (Z:.h)
-  {-# INLINE streamDown #-}
-
-instance IndexStream (PInt C p) where
-  streamUp l h = map (\(Z:.i) -> i) $ streamUp (Z:.l) (Z:.h)
-  {-# INLINE streamUp #-}
-  streamDown l h = map (\(Z:.i) -> i) $ streamDown (Z:.l) (Z:.h)
-  {-# INLINE streamDown #-}
-
-{-
-instance IndexStream z => IndexStream (z:.(PInt p)) where
-  streamUp (ls:.l) (hs:.h) = flatten mk step $ streamUp ls hs
-    where mk z = return (z,l)
-          step (z,k)
-            | k > h     = return $ Done
-            | otherwise = return $ Yield (z:.k) (z,k+1)
-          {-# Inline [0] mk   #-}
-          {-# Inline [0] step #-}
-  {-# Inline streamUp #-}
-  streamDown (ls:.l) (hs:.h) = flatten mk step $ streamDown ls hs
-    where mk z = return (z,h)
-          step (z,k)
-            | k < l     = return $ Done
-            | otherwise = return $ Yield (z:.k) (z,k-1)
-          {-# Inline [0] mk   #-}
-          {-# Inline [0] step #-}
-  {-# Inline streamDown #-}
-
-instance IndexStream (PInt p) where
-  streamUp l h = map (\(Z:.k) -> k) $ streamUp (Z:.l) (Z:.h)
-  {-# Inline streamUp #-}
-  streamDown l h = map (\(Z:.k) -> k) $ streamDown (Z:.l) (Z:.h)
-  {-# Inline streamDown #-}
--}
 
