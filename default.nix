4f2a24fa8358af7d0117a4c2baa77a3963344aa1
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-05-25 16:04:11 +0200

    testing yet another way of doing hs-dev

--- default.nix
+++ default.nix
@@ -1,6 +1,15 @@
-with import <nixpkgs> {};
-haskellPackages.extend (haskell.lib.packageSourceOverrides {
-  DPutils = ../Lib-DPutils;
-  OrderedBits = ../Lib-OrderedBits;
-  PrimitiveArray = ../Lib-PrimitiveArray;
-})
+with (import <nixpkgs> {});
+with haskell.lib;
+
+rec {
+  hsPkgs = haskellPackages.extend (packageSourceOverrides {
+    DPutils = ../Lib-DPutils;
+    OrderedBits = ../Lib-OrderedBits;
+    PrimitiveArray = ../Lib-PrimitiveArray;
+  });
+  hsShell = hsPkgs.shellFor {
+    packages = p: [ p.DPutils p.OrderedBits p.PrimitiveArray ];
+    withHoogle = true;
+    buildInputs = [ cabal-install ghc ];
+  };
+}
--- shell.nix
+++
@@ -1,4 +0,0 @@
-(import ./.).shellFor {
-  packages = p: [ p.DPutils p.OrderedBits p.PrimitiveArray ];
-  withHoogle = true;
-}
