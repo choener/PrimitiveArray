463996f9d621ede034e2d5880ff7d8aac8d15ab6
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-08-20 10:28:52 +0200

    preparing new version of default.nix
    should be shell nix, actually

--- default.nix
+++ default.nix
@@ -1,19 +1,33 @@
-with (import <nixpkgs> {});
+{ pkgs ? <nixpkgs> }:
+with (import pkgs {});
 with haskell.lib;
+with builtins;
 
-rec {
-  hsSrcSet
-    =  (import ../Lib-DPutils).hsSrcSet
-    // (import ../Lib-OrderedBits).hsSrcSet
-    // {PrimitiveArray = ./.;};
+let
+  # check directories below this one
+  parentContent = readDir ./..;
+  # extract sibling folders that contain a default.nix file
+  parentDirs = filter (d: pathExists (./.. + ("/" + d + "/default.nix"))) (attrNames parentContent);
+  # construct set of names / source directories for override
+  hsSrcSet = listToAttrs (map (d: {name = "${d}"; value = ./.. + ("/" + d);}) parentDirs);
+  # extend the set of packages with source overrides
   hsPkgs = haskellPackages.extend (packageSourceOverrides hsSrcSet);
+  # name of this module
+  this = baseNameOf ./.;
+  traceds = x: y: builtins.trace (builtins.deepSeq x x) y;
+in
+
+{
   hsShell = with hsPkgs; shellFor {
-    packages = p: [ p.PrimitiveArray ];
+    packages = p: let v = p.vector;
+                  in  [ p."${this}" ]; # traceds (p."${this}".buildInputs) [ ];
     withHoogle = true;
     buildInputs = [
-      cabal-install ghc
-      DPutils
-      OrderedBits
+      cabal-install
     ];
   };
+  # nix-build -A hsBuild
+  # this shall build and put into ./result
+  # the result is a typical ./bin/; ./lib/ etc.
+  hsBuild = with hsPkgs; callCabal2nix "${this}" ./. {};
 }
