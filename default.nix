ee1473fc8b1483efebb5238a7bb759d868f87167
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-08-07 11:19:30 +0200

    updated nix env

--- default.nix
+++ default.nix
@@ -1,21 +1,30 @@
 { mkDerivation, aeson, base, binary, bits, cereal, cereal-vector
-, containers, deepseq, DPutils, hashable, lens, log-domain, mtl
-, OrderedBits, primitive, QuickCheck, smallcheck, stdenv, tasty
-, tasty-quickcheck, tasty-smallcheck, tasty-th, text, vector
-, vector-binary-instances, vector-th-unbox
+, containers, criterion, deepseq, DPutils, hashable, lens
+, log-domain, mtl, OrderedBits, primitive, QuickCheck, smallcheck
+, stdenv, tasty, tasty-quickcheck, tasty-smallcheck, tasty-th, text
+, vector, vector-binary-instances, vector-th-unbox
 }:
 mkDerivation {
   pname = "PrimitiveArray";
-  version = "0.9.0.0";
+  version = "0.9.1.1";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base binary bits cereal cereal-vector deepseq DPutils
-    hashable lens log-domain mtl OrderedBits primitive QuickCheck
-    smallcheck text vector vector-binary-instances vector-th-unbox
+    aeson base binary bits cereal cereal-vector containers deepseq
+    DPutils hashable lens log-domain mtl OrderedBits primitive
+    QuickCheck smallcheck text vector vector-binary-instances
+    vector-th-unbox
   ];
   testHaskellDepends = [
-    base containers QuickCheck smallcheck tasty tasty-quickcheck
-    tasty-smallcheck tasty-th
+    aeson base binary bits cereal cereal-vector containers deepseq
+    DPutils hashable lens log-domain mtl OrderedBits primitive
+    QuickCheck smallcheck tasty tasty-quickcheck tasty-smallcheck
+    tasty-th text vector vector-binary-instances vector-th-unbox
+  ];
+  benchmarkHaskellDepends = [
+    aeson base binary bits cereal cereal-vector containers criterion
+    deepseq DPutils hashable lens log-domain mtl OrderedBits primitive
+    QuickCheck smallcheck text vector vector-binary-instances
+    vector-th-unbox
   ];
   homepage = "https://github.com/choener/PrimitiveArray";
   description = "Efficient multidimensional arrays";
