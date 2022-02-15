f18183cf5028ae8d0c6f854356b2b2da2ea20ff5
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-02-15 11:44:49 +0100

    updated default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 { mkDerivation, aeson, base, binary, bits, cereal, cereal-vector
 , containers, criterion, deepseq, DPutils, hashable, hashtables
-, lens, log-domain, mtl, OrderedBits, primitive, QuickCheck
-, smallcheck, stdenv, tasty, tasty-quickcheck, tasty-smallcheck
-, tasty-th, text, unordered-containers, vector, vector-algorithms
+, lens, lib, log-domain, mtl, OrderedBits, primitive, QuickCheck
+, smallcheck, tasty, tasty-quickcheck, tasty-smallcheck, tasty-th
+, text, unordered-containers, vector, vector-algorithms
 , vector-binary-instances, vector-th-unbox
 }:
 mkDerivation {
   pname = "PrimitiveArray";
-  version = "0.10.0.0";
+  version = "0.10.2.0";
   src = ./.;
   libraryHaskellDepends = [
     aeson base binary bits cereal cereal-vector containers deepseq
@@ -30,5 +30,5 @@ mkDerivation {
   ];
   homepage = "https://github.com/choener/PrimitiveArray";
   description = "Efficient multidimensional arrays";
-  license = stdenv.lib.licenses.bsd3;
+  license = lib.licenses.bsd3;
 }
